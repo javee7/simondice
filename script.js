39bf239cbec8946d4ed17fec6f421142b07ab5c1
DiegoCaleroCosta <96126869+DiegoCaleroCosta@users.noreply.github.com>
2022-01-26 12:25:21 -0300

    Agregadas las rondas.

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ const Rojo = document.getElementById("Rojo");
 const Verde = document.getElementById("Verde");
 const Azul = document.getElementById("Azul");
 const Amarillo = document.getElementById("Amarillo");
-let audio = document.getElementById("audio");
-let audioboton1 = document.getElementById("audio1");
-let audioboton2 = document.getElementById("audio2");
-let audioboton3 = document.getElementById("audio3");
-let audioboton4 = document.getElementById("audio4");
-
+const Ronda = document.getElementById("Round")
 
 let Start = 0;
 let SecuenciaMaquina;
@@ -24,17 +19,20 @@ Rojo.addEventListener("click", CRojo);
 Verde.addEventListener("click", CVerde);
 Azul.addEventListener("click", CAzul);
 Amarillo.addEventListener("click", CAmarillo);
-
-
+/**
+ * Genera un numero aleatorio del 0 al 3
+ */
 function getRandomInt(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 }
-
 function CreateSequence(sequence){
   sequence = getRandomInt(0,3);
   return sequence;
 }
 
+/**
+ * Compara strings 
+ */
 function strcmp(a, b)
 {   
     for(let i = 0; i < a.length; i++){
@@ -44,7 +42,9 @@ function strcmp(a, b)
     }
     return 1;  
 }
-
+/**
+ * Inicia el juego
+ */
 function StartGAME(){
   Start = 1;
   Perdio = 0;
@@ -53,100 +53,90 @@ function StartGAME(){
   Secuencia()
 }
 
+/**
+ * Agrega el vector del usuario el valor ingresado y lo verifica. 
+ */
 function CRojo(){
   if(Start)
   {
     SecuenciaUser += NROJO
-    audioboton1.play();
     Secuencia(ComprobarValor());
-    
   }
   else{
     alert("Juego no iniciado");
   }
 }
-
 function CVerde(){
-
   if(Start)
   {
     SecuenciaUser += NVERDE
-    audioboton2.play();
     Secuencia(ComprobarValor());
-    
   }
   else{
     alert("Juego no iniciado");
   }
 }
-
 function CAmarillo(){
   if(Start)
   {
     SecuenciaUser += NAMARILLO
-    audioboton3.play();
     Secuencia(ComprobarValor());
-    
   }
   else{
     alert("Juego no iniciado");
   }
 }
-
 function CAzul(){
   if(Start)
   {
     SecuenciaUser += NAZUL
-    audioboton4.play();
     Secuencia(ComprobarValor());
-    
   }
   else{
     alert("Juego no iniciado");
   }
 }
 
+/**
+ * Ejecuta la secuencia guardada en SecuenciaMaquina[]
+ */
 function EjecutarSecuencia(){
   for(let i = 0; i < SecuenciaMaquina.length; i++){
     if(SecuenciaMaquina[i] == 0){
-      Rojo.style.brightness = 1000;
-      setTimeout(1000);
-      Rojo.style.brightness = 1;
+      //Rojo.style.backgroundColor = "black";
+      //Rojo.style.backgroundColor = "red";
       alert("Rojo")
-      
     }
 
     if(SecuenciaMaquina[i] == 1){
-      Verde.style.brightness = 1000;
-      setTimeout(1000);
-      Verde.style.brightness = 1;
+      //Verde.style.backgroundColor = "black";
+      //Verde.style.backgroundColor = "green";
       alert("Verde")
-     
     }
 
     if(SecuenciaMaquina[i] == 2){
-      Azul.style.brightness = 1000;
-      setTimeout(1000);
-      Azul.style.brightness = 1;
+      //Azul.style.backgroundColor = "black";
+      //Azul.style.backgroundColor = "blue"; 
       alert("Azul")
-      
     }
 
     if(SecuenciaMaquina[i] == 3){
-      Amarillo.style.brightness = 1000;
-      setTimeout(1000);
-      Amarillo.style.brightness = 1;
+      //Amarillo.style.backgroundColor = "black";
+      //Amarillo.style.backgroundColor = "yellow";
       alert("Amarillo")
-     
     }
   }
 }
 
+/**
+ * Avisa si el 
+ */
+
 function Secuencia(){
-  let lastIndex = SecuenciaUser.length;
   if (SecuenciaMaquina.length == SecuenciaUser.length) {
     if(strcmp(SecuenciaMaquina,SecuenciaUser)){
       SecuenciaMaquina += CreateSequence(SecuenciaMaquina);
+      Ronda.innerHTML = `Ronda: ${SecuenciaMaquina.length}`;
       EjecutarSecuencia(SecuenciaMaquina);
       SecuenciaUser = [];
     }
@@ -155,14 +145,16 @@ function Secuencia(){
     }
   }
   if(Perdio == 10){
-    audio.play();
-    alert("Perdiste!!");
+    alert(`Perdiste en ronda ${SecuenciaMaquina.length}!!`, );
     Start = 0;
   }
 
 }
 
-function ComprobarValor(){
+/**
+ * Comprueba si el valor ingresado por usuario es igual que el ingresado por la maquina
+ */
+function ComprobarValor(){ 
   for(let i = 0; i < SecuenciaUser.length; i++){
     if(SecuenciaUser[i] != SecuenciaMaquina[i]){
       Perdio = 10;
@@ -172,4 +164,3 @@ function ComprobarValor(){
   Perdio = 0
   return;
 }
-
