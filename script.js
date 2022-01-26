ebfef25a09bc507a16fcb399126416815441ef2d
DiegoCaleroCosta <96126869+DiegoCaleroCosta@users.noreply.github.com>
2022-01-26 10:03:49 -0300

    Juego funcionando (falta sonido y iluminación)

---
+++ script.js
@@ -0,0 +1,156 @@
+const Start_button = document.getElementById("start-button");
+const Rojo = document.getElementById("Rojo");
+const Verde = document.getElementById("Verde");
+const Azul = document.getElementById("Azul");
+const Amarillo = document.getElementById("Amarillo");
+
+let Start = 0;
+let SecuenciaMaquina;
+let SecuenciaUser;
+let NROJO = 0;
+let NVERDE = 1;
+let NAZUL = 2;
+let NAMARILLO = 3;
+let Perdio = 0;
+
+Start_button.addEventListener("click", StartGAME);
+Rojo.addEventListener("click", CRojo);
+Verde.addEventListener("click", CVerde);
+Azul.addEventListener("click", CAzul);
+Amarillo.addEventListener("click", CAmarillo);
+
+
+function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+function CreateSequence(sequence){
+  sequence = getRandomInt(0,3);
+  return sequence;
+}
+
+function strcmp(a, b)
+{   
+    for(let i = 0; i < a.length; i++){
+       if (a[i] != b[i]){
+          return 0;
+       }
+    }
+    return 1;  
+}
+
+function StartGAME(){
+  Start = 1;
+  Perdio = 0;
+  SecuenciaUser = [];
+  SecuenciaMaquina = [];
+  Secuencia()
+}
+
+function CRojo(){
+  if(Start)
+  {
+    SecuenciaUser += NROJO
+    Secuencia(ComprobarValor());
+  }
+  else{
+    alert("Juego no iniciado");
+  }
+}
+
+function CVerde(){
+
+  if(Start)
+  {
+    SecuenciaUser += NVERDE
+    Secuencia(ComprobarValor());
+  }
+  else{
+    alert("Juego no iniciado");
+  }
+}
+
+function CAmarillo(){
+  if(Start)
+  {
+    SecuenciaUser += NAMARILLO
+    Secuencia(ComprobarValor());
+  }
+  else{
+    alert("Juego no iniciado");
+  }
+}
+
+function CAzul(){
+  if(Start)
+  {
+    SecuenciaUser += NAZUL
+    Secuencia(ComprobarValor());
+  }
+  else{
+    alert("Juego no iniciado");
+  }
+}
+
+function EjecutarSecuencia(){
+  for(let i = 0; i < SecuenciaMaquina.length; i++){
+    if(SecuenciaMaquina[i] == 0){
+      Rojo.style.brightness = 1000;
+      setTimeout(1000);
+      Rojo.style.brightness = 1;
+      alert("Rojo")
+    }
+
+    if(SecuenciaMaquina[i] == 1){
+      Verde.style.brightness = 1000;
+      setTimeout(1000);
+      Verde.style.brightness = 1;
+      alert("Verde")
+    }
+
+    if(SecuenciaMaquina[i] == 2){
+      Azul.style.brightness = 1000;
+      setTimeout(1000);
+      Azul.style.brightness = 1;
+      alert("Azul")
+    }
+
+    if(SecuenciaMaquina[i] == 3){
+      Amarillo.style.brightness = 1000;
+      setTimeout(1000);
+      Amarillo.style.brightness = 1;
+      alert("Amarillo")
+    }
+  }
+}
+
+function Secuencia(){
+  let lastIndex = SecuenciaUser.length;
+  if (SecuenciaMaquina.length == SecuenciaUser.length) {
+    if(strcmp(SecuenciaMaquina,SecuenciaUser)){
+      SecuenciaMaquina += CreateSequence(SecuenciaMaquina);
+      EjecutarSecuencia(SecuenciaMaquina);
+      SecuenciaUser = [];
+    }
+    else {
+      Perdio = 10;
+    }
+  }
+  if(Perdio == 10){
+    alert("Perdiste!!");
+    Start = 0;
+  }
+
+}
+
+function ComprobarValor(){
+  for(let i = 0; i < SecuenciaUser.length; i++){
+    if(SecuenciaUser[i] != SecuenciaMaquina[i]){
+      Perdio = 10;
+      return
+    }
+  }
+  Perdio = 0
+  return;
+}
+
